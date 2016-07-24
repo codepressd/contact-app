ab3226c89620ffcb5f0dc7f8efac2c9bae6dc79b
Codepressd <creederone@gmail.com>
2016-07-24 11:53:42 -0700

    Everything is working except I can't update contact details for a second or third user

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-var Contacts = {
+	var Contacts = {
 
-	listOfContacts:[
-		
+		listOfContacts:[
+			
 
-	],
+		],
 
-	addContact: function (contact){
-		
-		this.listOfContacts.push(contact);
-		
-	},
+		addContact: function (contact){
+			
+			this.listOfContacts.push(contact);
+			
+		},
 
-	getContact: function(id){
-		return this.listOfContacts[id];
-	}
+		getContact: function(id){
+			return this.listOfContacts[id];
+		}
 
-};
-
-/*On page*/
+	};
 
 $(document).ready(function(){
+
+
+
+/*Takes information from form and stores it*/
+
+
 	$('form').on('submit', function(event){
 		var newContact = {};
 		event.preventDefault();
@@ -33,7 +37,22 @@ $(document).ready(function(){
 		
 		}
 
-		Contacts.addContact(newContact);
+		Contacts.addContact(newContact);// adds newContact to the Object
+		$('ul.list-contacts').append('<li>'+newContact.firstname+ ' ' +newContact.lastname+'<li>')
+
 		$('p > input').val('');
+	});
+
+/*Click on a name and show that contact's information*/
+
+	$('.list-contacts').on('click', function(event){
+
+		var $target = $(event.target);
+		var index = $target.index();
+
+		var contactInfo = Contacts.getContact(index);
+		$('.info-list > li').remove();
+		$('.info-list').append('<li> First name: '+contactInfo.firstname+ '</li><li> Last name: '+contactInfo.lastname+ '</li><li> Phone Number: '+contactInfo.phonenumber+ '</li><li> Street Address: '+contactInfo.address+ ' '+contactInfo.city+ ', '+contactInfo.state+'</li>');
+
 	});
 });
